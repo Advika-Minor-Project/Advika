7cedd0bdad730de6f034cd73d7fafd5661a765a2
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2021-11-07 21:06:30 +0530

    Update urls.py

--- advika/urls.py
+++ advika/urls.py
@@ -14,8 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blogs/',include("blogs.urls")),
+    path('',include("users.urls")),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"),
+         name="password_reset_complete"),
+
+    path('tracker/',include('tracker.urls')),
+    
+
 ]
+
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
